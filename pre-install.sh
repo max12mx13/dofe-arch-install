5258c82235d7d78683d1547fb89b81bd56ab63e1
Max A <maxpeterambaum@gmail.com>
2022-05-31 21:44:48 +0100

    encryption pt10 finale

--- pre-install.sh
+++ pre-install.sh
@@ -1,7 +1,7 @@
 encryptionpostinstall(){
 	sed -i '/HOOKS/s/block/block encrypt keyboard /' /mnt/etc/mkinitcpio.conf	
 	deviceuuid=$(blkid $1 | awk -F"[ ',]+" '{print $2}')
-	sed -i 'GRUB_CMDLINE_LINUX_DEFAULT=/s/quiet/cryptdevice=${deviceuuid}:root root=/dev/mapper/root' /mnt/etc/default/grub
+	sed -i -r "/GRUB_CMDLINE_LINUX_DEFAULT/s/quiet/quiet cryptdevice=${deviceuuid}:root root=\/dev\/mapper\/root/" /mnt/etc/default/grub
 	arch-chroot /mnt mkinitcpio -P
 	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 }
